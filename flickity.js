b0ef6a7c0c403dd9341123a00f9f7397427ccd65
vishant <vishant.96.sehrawat@gmail.com>
2022-07-29 21:20:20 +0530

    initia lcommit

---
+++ flickity.js
@@ -0,0 +1,20 @@
+// document.getElementsByClassName("my-slider");
+
+var slider = tns({
+    container: ".my-slider",
+    items: 1,
+    responsive: {
+        640: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 2,
+        },
+        700: {
+            gutter: 30,
+        },
+        900: {
+            items: 3,
+            gutter: 30,
+        },
+    },
+});
